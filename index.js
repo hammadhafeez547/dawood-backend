2eb5f710b7a6d7b1a9ee39272ae0bd1dcbcacbbf
shoaib-solangi <161812821+shoaib-solangi@users.noreply.github.com>
2025-06-14 21:19:45 -0700

    server issue updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 // Response headers middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://dawood-frontends.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
